d89461f20cc809b5c0e14776981834660d296752
Zorica Vukovic <zoka200015@gmail.com>
2024-04-21 16:57:47 +0200

    test

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ func doStuff() string {
 	return "I do stuff!"
 }
 
-func main() {
+func add(x uint, y uint) uint {
+	return x
+}
 
-	fmt.Println(doStuff())
+func main() {
+	var x uint = 9  //0000 1001
+	var y uint = 65 //0100 0001
+
+	fmt.Println(add(y, x))
 
 }
